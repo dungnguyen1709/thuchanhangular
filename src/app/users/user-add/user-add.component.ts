a6adb0f97eda9043cb3f137f9215bf3f40aad77e
dungnguyen1709 <56040352+dungnguyen1709@users.noreply.github.com>
2020-10-23 09:22:20 +0700

    clone form luan

---
+++ src/app/users/user-add/user-add.component.ts
@@ -0,0 +1,31 @@
+import {Component, OnInit, EventEmitter, Output} from '@angular/core';
+
+@Component({
+  selector: 'app-user-add',
+  templateUrl: './user-add.component.html',
+  styleUrls: ['./user-add.component.css']
+})
+export class UserAddComponent implements OnInit {
+
+  @Output() data = new EventEmitter();
+  name: string = '';
+  email: string = '';
+  group: string = '';
+  avatar: string = '';
+  role: number;
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  getData() {
+    let user = {
+      name: this.name,
+      email: this.email,
+      group: this.group,
+    }
+
+    this.data.emit(user);
+  }
+
+}
