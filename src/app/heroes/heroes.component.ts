8176d4d610d9737ab34ac7c3eb62b4f081de9690
dungnguyen1709 <56040352+dungnguyen1709@users.noreply.github.com>
2020-10-26 09:13:30 +0700

    hien thi danh sach

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import {Hero} from '../Hero';
+import { Hero } from '../hero';
+import {HEROES} from '../mock-hero';
+
 
 @Component({
   selector: 'app-heroes',
@@ -7,14 +9,16 @@ import {Hero} from '../Hero';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-  hero: Hero = {
-    id: 1,
-    name: 'Huy'
-  };
 
   constructor() { }
+  heroes = HEROES;
+
+  selectedHero: Hero;
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
 
   ngOnInit(): void {
   }
-
 }
---
+++ src/app/mock-hero.ts
@@ -0,0 +1,15 @@
+import {Hero} from './Hero';
+
+export const HEROES: Hero[] = [
+  {id: 2, name: 'Tobi'},
+  {id: 3, name: 'Tung'},
+  {id: 4, name: 'Tam'},
+  {id: 5, name: 'Minh'},
+  {id: 6, name: 'Tuyen'},
+  {id: 7, name: 'Binh'},
+  {id: 8, name: 'Vinh'},
+  {id: 9, name: 'Nam'},
+  {id: 10, name: 'Quan'},
+  {id: 11, name: 'Hieu'},
+  {id: 12, name: 'Hiep'},
+];
